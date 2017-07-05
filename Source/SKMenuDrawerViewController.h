fbdb0ad1e7988789553d0e5543bcaf6052a70a2f
Sean Kladek <sean@303software.com>
2017-07-05 09:00:41 -0600

    Initial project commit

---
+++ Source/SKMenuDrawerViewController.h
@@ -0,0 +1,19 @@
+//
+//  SKMenuDrawerViewController.h
+//  SKMenuDrawerViewController
+//
+//  Created by Sean on 7/5/17.
+//  Copyright © 2017 Sean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SKMenuDrawerViewController.
+FOUNDATION_EXPORT double SKMenuDrawerViewControllerVersionNumber;
+
+//! Project version string for SKMenuDrawerViewController.
+FOUNDATION_EXPORT const unsigned char SKMenuDrawerViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKMenuDrawerViewController/PublicHeader.h>
+
+
