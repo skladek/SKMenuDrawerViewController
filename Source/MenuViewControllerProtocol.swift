5c1b1b797962fd4f6a9aca2a73f37b369c187c95
Sean Kladek <sean@303software.com>
2018-04-30 12:49:24 -0600

    Added @objc tags.

--- Source/MenuViewControllerProtocol.swift
+++ Source/MenuViewControllerProtocol.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 /// Provides the methods that the menu view controller must adopt.
+@objc
 public protocol MenuViewControllerProtocol {
 
     /// Returns the view controller to be initially displayed in the content pane.
