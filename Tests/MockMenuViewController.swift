ddd830fa00f85431ce11de8dd2111d1a3ef1f9ea
Sean Kladek <sean@303software.com>
2018-09-14 17:13:18 -0600

    Added tests for the lifecycle methods

--- Tests/MockMenuViewController.swift
+++ Tests/MockMenuViewController.swift
@@ -2,11 +2,35 @@
 
 class MockMenuViewController: UIViewController, MenuViewControllerProtocol {
     var initialContentViewControllerCalled = false
+    var viewDidAppearCalled = false
+    var viewDidDisappearCalled = false
+    var viewWillAppearCalled = false
+    var viewWillDisappearCalled = false
 
     func initialContentViewController() -> UIViewController {
         initialContentViewControllerCalled = true
 
         return UIViewController()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        viewDidAppearCalled = true
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        viewDidDisappearCalled = true
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        viewWillAppearCalled = true
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        viewWillDisappearCalled = true
     }
 }
 
